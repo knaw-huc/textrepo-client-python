13ee603ed5c7249df06a99fd5a9ac0d4d4267bbf
HDJ <hayco@users.noreply.github.com>
2021-08-24 12:57:08 +0200

    up minor for new API function

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE', encoding='utf8') as f:
 
 setup(
     name='textrepo',
-    version='0.1.0',
+    version='0.2.0',
     description='client to access textrepo',
     long_description=readme,
     author='Bram Buitendijk',
