3ab170665cac2b49b5681f1347c84391701d637e
HDJ <hayco@users.noreply.github.com>
2021-08-24 10:40:25 +0200

    minor version up because of API name changes: camelCase -> snake_case

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE', encoding='utf8') as f:
 
 setup(
     name='textrepo',
-    version='0.0.1',
+    version='0.1.0',
     description='client to access textrepo',
     long_description=readme,
     author='Bram Buitendijk',
