fd234b5ce777984193b1b54b760187c1ae34dbbb
HDJ <hayco@users.noreply.github.com>
2021-08-25 09:41:53 +0200

    Use builtin assert
    This also fixes the suggestion that this method should be static as it did not use any 'self' reference. Now we properly use the test harness.

--- tests/textrepo_client_test.py
+++ tests/textrepo_client_test.py
@@ -6,7 +6,7 @@ from textrepo.client import TextRepoClient
 class TextRepoClientTestCase(unittest.TestCase):
     def test_init(self):
         client = TextRepoClient("http://example.org")
-        assert client is not None
+        self.assertIsNotNone(client)
 
 
 if __name__ == '__main__':
