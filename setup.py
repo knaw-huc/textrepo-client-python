7c14ccb818064cc3460d2e18a2978830a51f42c5
HDJ <hayco@users.noreply.github.com>
2021-08-25 14:02:45 +0200

    Up minor because of snake_case / camelCase changes

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE', encoding='utf8') as f:
 
 setup(
     name='textrepo',
-    version='0.3.0',
+    version='0.4.0',
     description='client to access textrepo',
     long_description=readme,
     author='Bram Buitendijk',
